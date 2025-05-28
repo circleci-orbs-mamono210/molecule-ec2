b646856023023186f3cd47f86793c5706b94ada3
Tomonori Matsumura <matsumura_t@yahoo.co.jp>
2025-05-28 09:02:35 +0000

    CI ログに pip ライブラリのリリース日を付与する処理を追加した refs #4906

---
+++ src/scripts/molecule-release-date.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+get_release_date() {
+    pkg=$1; ver=$2
+    curl -s "https://pypi.org/pypi/${pkg}/json" \
+    | jq -r --arg V "$ver" '.releases[$V][] | .upload_time_iso_8601' \
+    | sort \
+    | tail -n1
+}
+MOL_VER=$(molecule --version | awk '{print $2}')
+MOL_DATE=$(get_release_date molecule "$MOL_VER")
+echo "molecule $MOL_VER released at $MOL_DATE"
